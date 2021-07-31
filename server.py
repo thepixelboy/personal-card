3de5a3f614886f57bb9e9b2190d49e375e78604a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-07-31 15:22:29 +0200

    feat: rendering html files with flask

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return render_template("cv.html")
 
 
 if __name__ == "__main__":
